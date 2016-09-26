4007d4e89826af5eb2a27bff4a4f142857b83ec7
Thamizharasu <zarub2k@gmail.com>
2016-09-27 00:42:27 +0530

    Problem 3 is handled

---
+++ src/main/java/com/cloudskol/javahub/p03/Problem03.java
@@ -0,0 +1,45 @@
+package com.cloudskol.javahub.p03;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author tham
+ */
+
+public class Problem03 {
+    public void findNumberByAlphabets(String value) {
+        if (value == null || value.isEmpty()) {
+            return;
+        }
+
+        int total = 0;
+        char charValue;
+        StringBuilder numberSeries = new StringBuilder();
+        final char[] chars = value.toCharArray();
+        for (int i = 0; i <chars.length; i++) {
+            charValue = chars[i];
+            final int series = getNumberSeries(charValue);
+            numberSeries.append(series);
+            total += series;
+        }
+
+        System.out.println("Number series: " + numberSeries);
+        System.out.println("Total of the series: " + total);
+    }
+
+    private int getNumberSeries(char charValue) {
+        return alphaMap.get(String.valueOf(charValue));
+    }
+
+    static Map<String, Integer> alphaMap = new HashMap<String, Integer>(26);
+    static {
+        int index = 1;
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            alphaMap.put(String.valueOf(ch), index);
+            index++;
+        }
+
+        System.out.println(alphaMap);
+    }
+}
---
+++ src/test/java/com/cloudskol/javahub/p03/Problem03Test.java
@@ -0,0 +1,15 @@
+package com.cloudskol.javahub.p03;
+
+import org.junit.Test;
+
+/**
+ * @author tham
+ */
+
+public class Problem03Test {
+    private static final String VALUE = "tham";
+    @Test
+    public void testFindNumberByAlphabets() {
+        new Problem03().findNumberByAlphabets(VALUE);
+    }
+}
